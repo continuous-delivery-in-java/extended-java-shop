46685895d999928c37e4e18c40b6332d045c31b2
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-08 20:05:31 +0100

    test getting a product that doesn't exist

--- stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplicationIT.java
+++ stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplicationIT.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.co.danielbryant.djshopping.stockmanager.model.Stock;
 
@@ -14,6 +15,8 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = StockManagerApplication.class)
@@ -24,17 +27,28 @@ public class StockManagerApplicationIT {
 
     @Test
     public void listOfStocksProvidesFiveElements() {
-        final List<Stock> stocks = getStocks();
+        final List<Stock> stocks = asList(get("/stocks", Stock[].class));
         assertThat(stocks, hasSize(5));
     }
 
     @Test
-    public void skuForFirstStockIsAsExpected() {
-        final Stock firstStock = getStocks().get(0);
-        assertThat(firstStock.getSku(), is("12345678"));
+    public void canGetSpecificStock() {
+        final Stock stock = get("/stocks/1", Stock.class);
+        assertThat(stock.getSku(), is("12345678"));
+        assertThat(stock.getAmountAvailable(), is(5));
     }
 
-    private List<Stock> getStocks() {
-        return asList(restTemplate.getForObject("/stocks", Stock[].class));
+    @Test
+    public void canHandleNotFoundStock() {
+        final ResponseEntity<Stock> response = rawGet("/stocks/99999999", Stock.class);
+        assertThat(response.getStatusCode(), is(NOT_FOUND));
+    }
+
+    private <T> T get(String path, Class<T> responseType) {
+        return restTemplate.getForObject(path, responseType);
+    }
+
+    private <T> ResponseEntity<T> rawGet(String path, Class<T> responseType) {
+        return restTemplate.exchange(path, GET, null, responseType);
     }
 }
