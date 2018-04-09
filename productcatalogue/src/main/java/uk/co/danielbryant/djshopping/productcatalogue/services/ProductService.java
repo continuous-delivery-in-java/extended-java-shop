6784dcc073a598ca9bbb24c34dfc201ef97a4618
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 18:19:13 +0100

    use string constructor for BigDecimal (this prevents precision issues)

--- productcatalogue/src/main/java/uk/co/danielbryant/djshopping/productcatalogue/services/ProductService.java
+++ productcatalogue/src/main/java/uk/co/danielbryant/djshopping/productcatalogue/services/ProductService.java
@@ -11,11 +11,11 @@ public class ProductService {
     private Map<String, Product> fakeProductDAO = new HashMap<>();
 
     public ProductService() {
-        fakeProductDAO.put("1", new Product("1", "Widget", "Premium ACME Widgets", new BigDecimal(1.20)));
-        fakeProductDAO.put("2", new Product("2", "Sprocket", "Grade B sprockets", new BigDecimal(4.10)));
-        fakeProductDAO.put("3", new Product("3", "Anvil", "Large Anvils", new BigDecimal(45.50)));
-        fakeProductDAO.put("4", new Product("4", "Cogs", "Grade Y cogs", new BigDecimal(1.80)));
-        fakeProductDAO.put("5", new Product("5", "Multitool", "Multitools", new BigDecimal(154.10)));
+        fakeProductDAO.put("1", new Product("1", "Widget", "Premium ACME Widgets", new BigDecimal("1.20")));
+        fakeProductDAO.put("2", new Product("2", "Sprocket", "Grade B sprockets", new BigDecimal("4.10")));
+        fakeProductDAO.put("3", new Product("3", "Anvil", "Large Anvils", new BigDecimal("45.50")));
+        fakeProductDAO.put("4", new Product("4", "Cogs", "Grade Y cogs", new BigDecimal("1.80")));
+        fakeProductDAO.put("5", new Product("5", "Multitool", "Multitools", new BigDecimal("154.10")));
     }
 
     public List<Product> getAllProducts() {
