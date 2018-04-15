b021ef8dbd4f72d223e6373d65a71f9fb6c982eb
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-15 20:39:12 +0100

    stop checking if the flag is already there (it will never be)

--- featureflags/src/main/resources/data-postgresql.sql
+++ featureflags/src/main/resources/data-postgresql.sql
@@ -1,6 +1,2 @@
--- Ensure the Adaptive Pricing Flag is always there when this service starts
-INSERT INTO flag(flagId, name, portionIn)
-    SELECT 1, 'adaptive-pricing', 50
-WHERE NOT EXISTS (
-    SELECT 1 FROM flag WHERE flagId = 1
-);
+-- Ensure the Adaptive Pricing Flag is added when the schema is created
+INSERT INTO flag(flag_id, name, portion_in) VALUES (1, 'adaptive-pricing', 50)
