7afc4dc121cc252690ec1b44ac7a873908ebfa27
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-17 21:39:37 +0100

    add integration tests to verify fault tolerance of FeatureFlagsRepo

--- shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/repo/FeatureFlagsRepo.java
+++ shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/repo/FeatureFlagsRepo.java
@@ -4,7 +4,11 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import uk.co.danielbryant.djshopping.shopfront.services.dto.FlagDTO;
 
@@ -23,9 +27,23 @@ public class FeatureFlagsRepo {
     @Qualifier(value = "stdRestTemplate")
     private RestTemplate restTemplate;
 
+    public FeatureFlagsRepo() {
+        // Needed by Spring
+    }
+
+    FeatureFlagsRepo(String featureFlagsUri, RestTemplate restTemplate) {
+        this.featureFlagsUri = featureFlagsUri;
+        this.restTemplate = restTemplate;
+    }
+
     public Optional<FlagDTO> getFlag(long flagId) {
-        final String flagUrl = featureFlagsUri + "/flags/" + flagId;
-        LOGGER.info("Fetching flag from {}", flagUrl);
-        return Optional.ofNullable(restTemplate.getForObject(flagUrl, FlagDTO.class));
+        try {
+            final String flagUrl = featureFlagsUri + "/flags/" + flagId;
+            LOGGER.info("Fetching flag from {}", flagUrl);
+            return Optional.ofNullable(restTemplate.getForObject(flagUrl, FlagDTO.class));
+        } catch (HttpClientErrorException | HttpServerErrorException | ResourceAccessException | HttpMessageNotReadableException e) {
+            LOGGER.info("Failed to retrieve flag " + flagId + "; falling back to no flag", e);
+            return Optional.empty();
+        }
     }
 }
