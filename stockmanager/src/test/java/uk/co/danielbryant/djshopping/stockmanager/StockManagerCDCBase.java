3f2c25a07ee20a624c95cb9f020b54b7fd57b625
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-18 20:31:04 +0100

    adjust implementation to abide by new contract

--- stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerCDCBase.java
+++ stockmanager/src/test/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerCDCBase.java
@@ -7,6 +7,10 @@ import uk.co.danielbryant.djshopping.stockmanager.model.Stock;
 import uk.co.danielbryant.djshopping.stockmanager.resources.StockResource;
 import uk.co.danielbryant.djshopping.stockmanager.services.StockService;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class StockManagerCDCBase {
@@ -20,7 +24,20 @@ public class StockManagerCDCBase {
     private class FakeStockService extends StockService {
         @Override
         public Stock getStock(String productId) throws StockNotFoundException {
-            return new Stock(productId, "sku-" + productId, 10);
+            return newStock(productId, 10);
+        }
+
+        @Override
+        public List<Stock> getStocks() {
+            return IntStream.rangeClosed(1, 100).mapToObj(this::newStock).collect(toList());
+        }
+
+        private Stock newStock(int i) {
+            return newStock(Integer.toString(i), i * 10);
+        }
+
+        private Stock newStock(String productId, int amountAvailable) {
+            return new Stock(productId, "sku-" + productId, amountAvailable);
         }
     }
 }
