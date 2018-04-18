f39ac048cda5e8385cc95209773562eae42986d1
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-18 20:45:14 +0100

    add CDC test for the new contract (get all stock)

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/StockManagerCDC.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/StockManagerCDC.java
@@ -3,21 +3,18 @@ package uk.co.danielbryant.djshopping.shopfront;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-import uk.co.danielbryant.djshopping.shopfront.repo.FeatureFlagsRepo;
 import uk.co.danielbryant.djshopping.shopfront.repo.StockRepo;
 import uk.co.danielbryant.djshopping.shopfront.services.dto.StockDTO;
 
+import java.util.Map;
+
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,9 +24,16 @@ public class StockManagerCDC {
     private StockRepo stockRepo;
 
     @Test
-    public void stockCanBeReadFromProducerResponse() {
+    public void canReadResponseWithIndividualStock() {
         final StockDTO expected = new StockDTO("123", "sku-123", 10);
         final StockDTO actual = stockRepo.getStockDTO("123");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canReadResponseWithAllStock() {
+        final Map<String, StockDTO> stockDTOs = stockRepo.getStockDTOs();
+        assertThat(stockDTOs.values(), hasSize(3));
+        assertThat(stockDTOs.keySet(), hasItems("1", "2", "3"));
     }
 }
