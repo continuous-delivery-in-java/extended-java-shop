27452c39fbc5f8a770e2ee799962f1557d767d10
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-18 20:10:07 +0100

    add new contract to the Stock Manager Service

---
+++ stockmanager/src/test/resources/contracts/stockmanager/getAllStock.groovy
@@ -0,0 +1,33 @@
+package contracts.stockmanager
+
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        method 'GET'
+        url '/stocks'
+    }
+    response {
+        status 200
+        body(
+                [
+                        [
+                                productId      : "1",
+                                sku            : "sku-1",
+                                amountAvailable: 10
+                        ],
+                        [
+                                productId      : "2",
+                                sku            : "sku-2",
+                                amountAvailable: 20
+                        ],
+                        [
+                                productId      : "3",
+                                sku            : "sku-3",
+                                amountAvailable: 30
+                        ]
+                ]
+        )
+        headers {
+            contentType('application/json')
+        }
+    }
+}
