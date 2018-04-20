874cbbe9d14b50f87ceb583921833db93a2c8966
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-20 11:38:04 +0100

    wait for docker services to be ready to start tests

--- acceptance-tests/src/test/java/com/github/quiram/shopping/acceptancetests/ShoppingAT.java
+++ acceptance-tests/src/test/java/com/github/quiram/shopping/acceptancetests/ShoppingAT.java
@@ -1,15 +1,39 @@
 package com.github.quiram.shopping.acceptancetests;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.core.Is.is;
 
 public class ShoppingAT {
-
     private static final ContentType CONTENT_TYPE = ContentType.JSON;
     private static final String SUT_BASE_URI = "http://localhost:8010/";
+
+    @BeforeClass
+    public static void checkServiceIsUp() {
+        await().atMost(2, MINUTES).until(ShoppingAT::serviceIsReady);
+    }
+
+    private static boolean serviceIsReady() {
+        final Response response;
+
+        try {
+            response = given().contentType(CONTENT_TYPE)
+                    .when()
+                    .get(SUT_BASE_URI)
+                    .thenReturn();
+        } catch (Exception e) {
+            return false;
+        }
+
+        return response.statusCode() == SC_OK;
+    }
 
     @Test
     public void correctNumberOfProductsReturned() {
