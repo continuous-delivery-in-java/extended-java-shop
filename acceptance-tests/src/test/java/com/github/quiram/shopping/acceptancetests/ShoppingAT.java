47c5f5217698e8a1992b4927b0a6aecd8c81cbfe
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-23 19:17:07 +0100

    implement first Acceptance Test using SerenityBDD

--- acceptance-tests/src/test/java/com/github/quiram/shopping/acceptancetests/ShoppingAT.java
+++ acceptance-tests/src/test/java/com/github/quiram/shopping/acceptancetests/ShoppingAT.java
@@ -1,68 +1,25 @@
 package com.github.quiram.shopping.acceptancetests;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.junit.BeforeClass;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static io.restassured.RestAssured.given;
-import static java.lang.String.format;
-import static java.util.concurrent.TimeUnit.MINUTES;
-import static org.apache.http.HttpStatus.SC_OK;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.core.Is.is;
-
+@RunWith(SerenityRunner.class)
 public class ShoppingAT {
-    private static final ContentType CONTENT_TYPE = ContentType.JSON;
-    private static final String SHOPFRONT_IP = System.getenv("shopfront_ip");
-    private static final String SUT_BASE_URI = format("http://%s:8010/", SHOPFRONT_IP);
-
-    @BeforeClass
-    public static void checkServiceIsUp() {
-        await().atMost(3, MINUTES).until(ShoppingAT::serviceIsReady);
-    }
-
-    private static boolean serviceIsReady() {
-        final Response response;
-
-        try {
-            response = given().contentType(CONTENT_TYPE)
-                    .when()
-                    .get(SUT_BASE_URI)
-                    .thenReturn();
-        } catch (Exception e) {
-            return false;
-        }
-
-        return response.statusCode() == SC_OK;
-    }
+    @Steps
+    private
+    ShoppingSteps steps;
 
     @Test
-    public void correctNumberOfProductsReturned() {
-        given().contentType(CONTENT_TYPE)
-                .when()
-                .get(SUT_BASE_URI + "products")
-                .then()
-                .body("size()", is(5));
-    }
+    public void numberOfProductsAsExpected() {
+        // GIVEN
+        steps.shopfront_service_is_ready();
 
-    @Test
-    public void productOneHasCorrectProductInfo() {
-        given().contentType(CONTENT_TYPE)
-                .when()
-                .get(SUT_BASE_URI + "products")
-                .then()
-                .body("[0].id", is("1"))
-                .body("[0].sku", is("12345678"));
-    }
+        // WHEN
+        steps.user_obtains_the_list_of_products();
 
-    @Test
-    public void productOneHasCorrectStockInfo() {
-        given()
-                .contentType(CONTENT_TYPE)
-                .when()
-                .get(SUT_BASE_URI + "products")
-                .then()
-                .body("[0].amountAvailable", is(5));
+        // THEN
+        steps.product_list_has_size(5);
     }
 }
