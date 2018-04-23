d6c34fc20c32dbb6ff2def4006de0d619a58cef4
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-23 17:52:37 +0100

    use shorter reference

--- stockmanager/src/test/resources/contracts/stockmanager/getAllStock.groovy
+++ stockmanager/src/test/resources/contracts/stockmanager/getAllStock.groovy
@@ -1,6 +1,8 @@
 package contracts.stockmanager
 
-org.springframework.cloud.contract.spec.Contract.make {
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
     request {
         method 'GET'
         url '/stocks'
