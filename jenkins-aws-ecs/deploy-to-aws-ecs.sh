588833d6a1c12ce7643cb5281363f86b95e86012
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-26 21:22:20 +0100

    appropriately identify new and existing AWS ECS services

--- jenkins-aws-ecs/deploy-to-aws-ecs.sh
+++ jenkins-aws-ecs/deploy-to-aws-ecs.sh
@@ -26,13 +26,13 @@ REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${
 
 #Create or update service
 if [ "$SERVICES" == "" ]; then
-    echo "entered new service"
-    aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
-else
     echo "entered existing service"
     DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
     if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
     fi
     aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
+else
+    echo "entered new service"
+    aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
 fi
