02b479bc2b478009976205c10f57a6b21fe195a2
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-27 19:33:30 +0100

    simplify the deployment mechanism (ignore docker tags for now)

--- jenkins-aws-ecs/deploy-to-aws-ecs.sh
+++ jenkins-aws-ecs/deploy-to-aws-ecs.sh
@@ -7,22 +7,17 @@ cp jenkins-aws-ecs/task-definitions/${project_name}-task.json taskdef.json
 #Constants
 
 REGION=eu-west-2
-REPOSITORY_NAME=quiram/${project_name}
 CLUSTER=extended-java-shop
 FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
-NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
-SERVICE_NAME=${NAME}-service
-
-#Store the repositoryUri as a variable
-REPOSITORY_URI=https://hub.docker.com/r/${REPOSITORY_NAME}
+SERVICE_NAME=${project_name}-service
 
 #Replace the build number and respository URI placeholders with the constants above
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
+cp taskdef.json ${project_name}-v_${BUILD_NUMBER}.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
 #Get latest revision
-REVISION=`aws ecs describe-task-definition --task-definition ${NAME} --region ${REGION} | jq .taskDefinition.revision`
+REVISION=`aws ecs describe-task-definition --task-definition ${project_name} --region ${REGION} | jq .taskDefinition.revision`
 
 #Create or update service
 if [ "$SERVICES" == "" ]; then
