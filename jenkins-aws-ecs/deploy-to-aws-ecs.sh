6439fb2f121a0e44c0a037a785d5e4f7c7ad4b20
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-05-02 11:31:42 +0100

    correct typos

--- jenkins-aws-ecs/deploy-to-aws-ecs.sh
+++ jenkins-aws-ecs/deploy-to-aws-ecs.sh
@@ -4,7 +4,7 @@ echo "Deploying service '${project_name}'"
 
 cp jenkins-aws-ecs/task-definitions/${project_name}-task.json taskdef.json
 
-# The below has been dapted from https://docs.aws.amazon.com/AWSGettingStartedContinuousDeliveryPipeline/latest/GettingStarted/CICD_Jenkins_Pipeline.html
+# The below has been adapted from https://docs.aws.amazon.com/AWSGettingStartedContinuousDeliveryPipeline/latest/GettingStarted/CICD_Jenkins_Pipeline.html
 
 #Load Constants
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -16,7 +16,7 @@ popd
 FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
 SERVICE_NAME=${project_name}-service
 
-#Replace the build number and respository URI placeholders with the constants above
+#Rename the task definition file to reflect version
 cp taskdef.json ${project_name}-v_${BUILD_NUMBER}.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${project_name}-v_${BUILD_NUMBER}.json --region ${REGION}
